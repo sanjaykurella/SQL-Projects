0208c9fba1591676a7250a903a51d5bd14413418
sanjaykurella <sanjaykurella58@gmail.com>
2026-08-05 18:52:21 +0530

    Add schema.sql

---
+++ 01-Hyderabad-Metro-Analytics/SQL/schema.sql
@@ -0,0 +1,13 @@
+/*
+=========================================================
+Project : Hyderabad Metro Ridership & Operations Analytics
+Author  : Kurella Sanjay
+Database: PostgreSQL
+=========================================================
+
+Description:
+This script creates the database schema for the Hyderabad Metro Analytics project.
+
+Status:
+Initial Version
+*/
